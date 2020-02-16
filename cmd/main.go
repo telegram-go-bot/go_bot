224f451449b9c52f682e6f22bb3e1322bf562411
azerg <pub@azerg.com>
2020-02-17 00:09:44 +0200

    m.f.

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"net/http"
 	"os"
 	"strings"
-	"time"
 
 	activityhandlers "github.com/telegram-go-bot/go_bot/app/activity_handlers"
 	onpickfirstorsecond "github.com/telegram-go-bot/go_bot/app/activity_handlers/on_pick_first_or_second"
@@ -21,7 +19,6 @@ var (
 	baseURL    = os.Getenv("HEROKU_BASE_URL")
 	dbURL      = os.Getenv("DATABASE_URL")
 	botUuids   = strings.Split(os.Getenv("BOT_UIDS"), ",")
-	rnd        = rand.New(rand.NewSource(time.Now().UnixNano()))
 	vkLogin    = os.Getenv("VK_LOGIN")
 	vkPwd      = os.Getenv("VK_PASSWORD")
 	botAdminID = os.Getenv("OWNER_ID")
